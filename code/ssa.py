53ebb72fd5b4a874a56f0518a603d3d8fb2ccd65
Don Pugeau <dpugeau@gmail.com>
2020-07-03 13:05:07 -0400

    Added logic to not snapshot if one in process

--- code/ssa.py
+++ code/ssa.py
@@ -18,6 +18,10 @@ def filter_instances(project):
 
     return instances
 
+def has_pending_snapshot(volume):
+    snapshots = list(volume.snapshots.all())
+    return snapshots and snapshots[0].state == 'pending'
+
 @click.group()
 def cli():
     """ssa manages snapshots"""
@@ -156,6 +160,9 @@ def create_snapshots(project):
         i.wait_until_stopped()
         for v in i.volumes.all():
             print('Starting snapshot of volume ID: {0} ...'.format(v.id))
+            if has_pending_snapshot(v):
+                print('Skipping..., Snapshot already in progress for volume {0}'.format(v.id))
+                continue
             v.create_snapshot(Description='Created by SnapshotAnalyzer')
         print('Starting instance...')
         i.start()
